734edee976f7eceded39d053b5f2507d5fd88366
mikeangelo6996 <57066432+mikeangelo6996@users.noreply.github.com>
2020-07-14 23:25:16 +0100

    ip

--- churchproject/settings.py
+++ churchproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['208.68.36.31']
 
 
 # Application definition
