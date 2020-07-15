5450a2693597b832aed9bf00d79bfcaaed890df2
mikeangelo6996 <57066432+mikeangelo6996@users.noreply.github.com>
2020-07-15 13:45:45 +0100

    database

--- churchproject/settings.py
+++ churchproject/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'churchproject.wsgi.application'
 #     }
 # }
 # else:
-    DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'churchproject',
