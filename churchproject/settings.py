9ea6a0e5fa8ea0c0f63ea9a762d55d536906477f
mikeangelo6996 <57066432+mikeangelo6996@users.noreply.github.com>
2020-07-15 13:44:08 +0100

    database

--- churchproject/settings.py
+++ churchproject/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'churchproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-else:
+# if DEBUG:
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# else:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
