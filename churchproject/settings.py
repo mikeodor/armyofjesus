2243233ba279e5cd639affe04d1df0d0b9cf9fcf
mikeangelo6996 <57066432+mikeangelo6996@users.noreply.github.com>
2020-07-15 13:41:26 +0100

    o

--- churchproject/settings.py
+++ churchproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -141,17 +140,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL='/media/'
 STATICFILES_DIRS = [
 
     os.path.join(BASE_DIR, 'static')
 
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assest/')
-
-
-
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-MEDIA_URL='/media/'
+
 
 
 
